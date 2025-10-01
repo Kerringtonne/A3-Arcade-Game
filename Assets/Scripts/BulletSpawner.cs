cdc48071ec6b9094bb78fea7bfdbd8e2a140fb7d
Kelly Chau <s5433155@student.ad.griffith.edu.au>
2025-10-01 14:58:29 +1000

    Week 11 tutorial work session
    - added new pattern
    - patterns change after a certain amount of time

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class BulletSpawner : MonoBehaviour
 {
-    enum SpawnerType { Straight, Spin }
+    enum SpawnerType { Spin, Spray }
 
     [Header("Bullet Attributes")]
     public GameObject bullet;
@@ -24,17 +24,32 @@ public class BulletSpawner : MonoBehaviour
             spawnedBullet = Instantiate (bullet, transform.position, Quaternion.identity);
             spawnedBullet.GetComponent <Bullet>().speed = bulletSpeed;
             spawnedBullet.transform.rotation = transform.rotation;
+
+            spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            spawnedBullet.GetComponent<Bullet>().speed = -bulletSpeed;
+            spawnedBullet.transform.rotation = transform.rotation;
         }
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(10);
+        spawnerType = SpawnerType.Spray; // switches to Spray
+        yield return new WaitForSeconds(10);
+        spawnerType = SpawnerType.Spin; // switches back to Spin
     }
 
     void Update()
     {
         timer += Time.deltaTime;
         if (spawnerType == SpawnerType.Spin) transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.z + rotationSpeed * Time.deltaTime);
+        if (spawnerType == SpawnerType.Spray) transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + rotationSpeed * Time.deltaTime * -2);
         if (timer >= firingRate)
         {
             Fire();
             timer = 0f;
         }
+
+
     }
 }
