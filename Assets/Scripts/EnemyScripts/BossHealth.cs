39a76e88c2bdc8a0069a3a1b643ff0df486ae0f1
Kerringtonne <kchau4400@gmail.com>
2025-10-14 18:45:15 +1000

    postsubmission extra stuff
    - player/boss changes colour on hit
    - bullets now have trail

--- Assets/Scripts/EnemyScripts/BossHealth.cs
+++ Assets/Scripts/EnemyScripts/BossHealth.cs
@@ -9,16 +9,25 @@ public class BossHealth : MonoBehaviour
     public int currentHealth;
     public TextMeshProUGUI healthText;
     public LoadScene loadSceneScript;
+    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer2;
+    public Color32 hitColour = new(48, 236, 255, 255);
+    public float hitSeconds = 1;
+
+    private Color originalColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        originalColor = spriteRenderer.color;
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        StartCoroutine(BossHit());
+
         if (currentHealth <= 0)
         {
             currentHealth = 0;
@@ -33,6 +42,18 @@ public class BossHealth : MonoBehaviour
             {
                 loadSceneScript.GoToScene();
             }
+        }
+    }
+
+    public IEnumerator BossHit()
+    {
+        if (currentHealth < maxHealth)
+        {
+            spriteRenderer.color = hitColour;
+            spriteRenderer2.color = hitColour;
+            yield return new WaitForSeconds(hitSeconds);
+            spriteRenderer.color = originalColor;
+            spriteRenderer2.color = originalColor;
         }
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,25 +1,44 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 3;
+    public int maxHealth = 10;
     public int currentHealth;
     public TextMeshProUGUI healthText;
     public string sceneName = "Scene";
+    public SpriteRenderer spriteRenderer;
+    public Color32 hitColour = new(48, 236, 255, 255);
+    public float hitSeconds = 1;
+
+    private Color originalColor;
 
     void Start()
     {
         currentHealth = maxHealth;
+        originalColor = spriteRenderer.color;
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        StartCoroutine(PlayerHit());
+
         if (currentHealth <= 0)
         {
             SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    public IEnumerator PlayerHit()
+    {
+        if (currentHealth < maxHealth)
+        {
+            spriteRenderer.color = hitColour;
+            yield return new WaitForSeconds(hitSeconds);
+            spriteRenderer.color = originalColor;
         }
     }
 
